684223311da16a9c2b99d2e2642ba0ab569dd260
Kaio HSG <kaio12hsg@gmail.com>
2026-01-13 17:07:15 -0300

    SPDX license identifier added

--- .frontier/src/backend.rs
+++ .frontier/src/backend.rs
@@ -1,6 +1,5 @@
-// Copyright 2026 The Frontier Framework Authors. All rights reserved.
-// Use of this source code is governed by an Apache-2.0 license 
-// that can be found in the LICENSE file.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 The Frontier Framework Authors
 
 // Backend Processing Module
 // 
